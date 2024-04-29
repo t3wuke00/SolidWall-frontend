ab98a7691e6ed9e3f2150a1a1f84d694d6ec68d0
mahboba <mithimithila177@gmail.com>
2024-04-29 06:08:10 +0300

    added some css and html for further implement

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// JavaScript function to toggle visibility of phone number
+function togglePhoneNumber() {
+    var phoneNumber = document.getElementById('phone-number');
+    phoneNumber.style.display = (phoneNumber.style.display === 'none') ? 'block' : 'none';
+}
